4ca00dcf60a86b727e39bd76b599cfbd7320b0bd
ark231 <arklp20909@gmail.com>
2022-01-29 20:23:27 +0900

    add: cmakeのプロジェクトのバージョン情報

--- Wuerfel_Qt_ja_JP.ts
+++ Wuerfel_Qt_ja_JP.ts
@@ -16,7 +16,7 @@
     </message>
     <message>
         <location filename="mainwindow.ui" line="118"/>
-        <location filename="mainwindow.cpp" line="218"/>
+        <location filename="mainwindow.cpp" line="229"/>
         <source>int</source>
         <translation>整数</translation>
     </message>
@@ -26,7 +26,7 @@
     </message>
     <message>
         <location filename="mainwindow.ui" line="123"/>
-        <location filename="mainwindow.cpp" line="220"/>
+        <location filename="mainwindow.cpp" line="231"/>
         <source>real</source>
         <translation>実数</translation>
     </message>
@@ -74,114 +74,124 @@
         <translation>乱数生成モード</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="39"/>
+        <location filename="mainwindow.cpp" line="45"/>
         <source>mkdir failed</source>
         <translation>ディレクトリ作成失敗</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="40"/>
+        <location filename="mainwindow.cpp" line="46"/>
         <source>error: couldn&apos;t create path &quot;%1&quot;.</source>
         <translation>エラー: &quot;%1&quot;というパスを作成できませんでした。</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="54"/>
+        <location filename="mainwindow.cpp" line="56"/>
+        <source>cp failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="57"/>
+        <source>error: couldn&apos;t copy template file &quot;%1&quot;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="63"/>
         <source>chmod failed</source>
         <translation>権限変更失敗</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="55"/>
+        <location filename="mainwindow.cpp" line="64"/>
         <source>error: couldn&apos;t change permissions of &quot;%1&quot;.</source>
         <translation>エラー: &quot;%1&quot;というファイルの権限を変更できませんでした。</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="60"/>
+        <location filename="mainwindow.cpp" line="70"/>
         <source>template file open error</source>
         <translation>テンプレートファイルオープン失敗</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="61"/>
+        <location filename="mainwindow.cpp" line="71"/>
         <source>error: couldn&apos;t open template file %1</source>
         <translation>エラー: &quot;%1&quot;という名前のテンプレートファイルを開くことができませんでした</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="70"/>
+        <location filename="mainwindow.cpp" line="81"/>
         <source>enter choice</source>
         <translation>選択肢を入力</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="70"/>
+        <location filename="mainwindow.cpp" line="81"/>
         <source>new choice:</source>
         <translation>追加する選択肢:</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="90"/>
+        <location filename="mainwindow.cpp" line="101"/>
         <source>invalid number error</source>
         <translation>不的確値エラー</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="91"/>
+        <location filename="mainwindow.cpp" line="102"/>
         <source>error: invalid number. please try again</source>
         <translation>エラー: 不的確な目が出ました。もう一度振り直してください</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="131"/>
+        <location filename="mainwindow.cpp" line="142"/>
         <source>open template</source>
         <translation>テンプレートを開く</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="131"/>
+        <location filename="mainwindow.cpp" line="142"/>
         <source>choose template name</source>
         <translation>テンプレート名を選択</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="137"/>
+        <location filename="mainwindow.cpp" line="148"/>
         <source>template open error</source>
         <translation>テンプレート読み込み失敗</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="138"/>
+        <location filename="mainwindow.cpp" line="149"/>
         <source>error: couldn&apos;t open template &quot;%1&quot;. returned value wasn&apos;t array</source>
         <translation>エラー: &quot;%1&quot;という名前のテンプレートを開くことができませんでした。返り値が配列ではありませんでした</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="152"/>
+        <location filename="mainwindow.cpp" line="163"/>
         <source>save template</source>
         <translation>テンプレートを保存</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="152"/>
+        <location filename="mainwindow.cpp" line="163"/>
         <source>enter template title</source>
         <translation>テンプレートのタイトルを入力してください</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="164"/>
+        <location filename="mainwindow.cpp" line="175"/>
         <source>template already exists</source>
         <translation>テンプレートがすでに存在します</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="165"/>
+        <location filename="mainwindow.cpp" line="176"/>
         <source>template with given name already exists. do you want to overwrite it?</source>
         <translation>与えられた名前のテンプレートがすでに存在します。上書きしますか？</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="181"/>
-        <location filename="mainwindow.cpp" line="202"/>
+        <location filename="mainwindow.cpp" line="192"/>
+        <location filename="mainwindow.cpp" line="213"/>
         <source>template saveerror</source>
         <translation>テンプレート保存失敗</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="181"/>
-        <location filename="mainwindow.cpp" line="202"/>
+        <location filename="mainwindow.cpp" line="192"/>
+        <location filename="mainwindow.cpp" line="213"/>
         <source>error: couldn&apos;t write template to file</source>
         <translation>エラー: テンプレートをファイルに保存することができませんでした</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="192"/>
+        <location filename="mainwindow.cpp" line="203"/>
         <source>delete template</source>
         <translation>テンプレート削除</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="192"/>
+        <location filename="mainwindow.cpp" line="203"/>
         <source>choose template name to delete</source>
         <translation>削除するテンプレート名を選択</translation>
     </message>
